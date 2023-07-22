5fac5c35a165304a1ba12ceea524994d646f96dc
sarath <sarathkp1998@gmail.com>
2023-07-22 19:04:07 +0530

    adding home route

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -3,14 +3,15 @@ import { Button } from "@mui/material"
 function Home() {
     return (
         <div>Home <br />
-            <Button size={"large"}
+            {/* <Button size={"large"}
                 variant="contained" onClick={() => {
                     window.location = '/Signup'
                 }}>Sign Up</Button>
             <Button size={"large"}
                 variant="contained" onClick={() => {
                     window.location = '/Login'
-                }}>Login</Button>
+                }}>Login</Button> */}
+                Welcome to course selling appp
 
         </div>
 
