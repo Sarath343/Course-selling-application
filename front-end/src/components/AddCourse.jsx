3627fd2e4211de7bae0f72026fc0f0fd891f539a
sarath <sarathkp1998@gmail.com>
2023-07-28 20:59:39 +0530

    add course page

--- front-end/src/components/AddCourse.jsx
+++ front-end/src/components/AddCourse.jsx
@@ -1,146 +1,70 @@
 import { Typography, Card, Grid, TextField, Button } from "@mui/material";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BASE_URL } from '../config'
 function AddCourse() {
-    const { courseId } = useParams();
-    const [course, setCourse] = useState(null);
-   
-    if (course) {
-        return (
-            <Card>
-                <GrayTopper title={course.title} />
-                <Grid container>
-                    <Grid item lg={8} md={12} sm={12}>
-                        <UpdateCard course={course} setCourse={setCourse} />
-                    </Grid>
-                    <Grid item lg={4} md={12} sm={12}>
-                        <CourseCard course={course} />
-                    </Grid>
-                </Grid>
-            </Card>
-        )
-    }
-    else {
-        return <div div style={{ height: "100vh", justifyContent: "center", flexDirection: "column" }} >Loading. . . </div>
-    }
-}
-function UpdateCard({ course, setCourse }) {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [imageLink, setImage] = useState("");
+    const [title, setName] = useState("");
+    const [description, setDesc] = useState("");
     const [price, setPrice] = useState("");
-    return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-
-            <Card varint={"outlined"} style={{ maxWidth: 600, marginTop: 200 }}>
-                <div style={{ padding: 20 }}>
-                    <Typography style={{ marginBottom: 10 }}>Update course details</Typography>
-                    <TextField
-                        value={title}
-                        style={{ marginBottom: 10 }}
-                        onChange={(e) => {
-                            setTitle(e.target.value)
-                        }}
-                        fullWidth={true}
-                        label="Title"
-                        variant="outlined"
-                    />
-                    <TextField
-                        value={description}
-                        style={{ marginBottom: 10 }}
-                        onChange={(e) => {
-                            setDescription(e.target.value)
-                        }}
-                        fullWidth={true}
-                        label="Description"
-                        variant="outlined"
-                    />
-                    <TextField
-                        value={imageLink}
-                        onChange={(e)=>{
-                            setImage(e.target.value)
-                        }}
-                        label="Image Link"
-                        variant="outlined"
-                        style={{marginBottom:10}}
-                        fullWidth={true}
-                    />
-                    <TextField
-                        value={price}
-                        style={{ marginBottom: 10 }}
-                        onChange={(e) => {
-                            setPrice(e.target.value)
-                        }}
-                        fullWidth={true}
-                        label="Price"
-                        variant="outlined"
-                    />
-                    <Button
-                        variant="contained"
-                        onClick={async () => {
-                           await  axios.put("http://localhost:4000/admin/updateCourse/" + course._id, {
-                                title: title,
-                                description: description,
-                                imageLink: imageLink,
-                                published: true,
-                                price
-                            }, {
-                                headers: {
-                                    "Content-type": "application/json",
-                                    "Authorization": "Bearer " + localStorage.getItem("token")
-                                }
-                            });
-                            let updatedCourse = {
-                                _id: course._id,
-                                title: title,
-                                description: description,
-                                imageLink: imageLink,
-                                price
-                            };
-                            setCourse(updatedCourse);
-                        }}
-                    > Update course</Button>
-                </div>
+    const [imageLink, setImagelink] = useState("")
+    //const [published,setPublished] = useState(false)
+    const navigate = useNavigate();
+    return <div
+        style={{ display: "flex", justifyContent: 'center', minHeight: '70vh', flexDirection: 'column' }}
+    >
+        <div style={{ display: "flex", justifyContent: 'center' }}>
+            <Card varint={"outlined"} style={{ width: 400, padding: 20, marginTop: 30, height: "100%" }}>
+ <TextField
+                    style={{ marginBottom: 10 }}
+                    fullWidth={true}
+                    label="Title"
+                    variant="outlined"
+                    onChange={(e) => {
+                        setName(e.target.value)
+                    }}></TextField>
+                <TextField
+                    style={{ marginBottom: 10 }}
+                    fullWidth={true}
+                    label="Despription"
+                    variant="outlined"
+                    onChange={(e) => {
+                        setDesc(e.target.value)
+                    }}></TextField>
+                <TextField
+                    style={{ marginBottom: 10 }}
+                    fullWidth={true}
+                    label="Price"
+                    variant="outlined"
+                    onChange={(e) => {
+                        setPrice(e.target.value)
+                    }}></TextField>
+                <TextField
+                    style={{ marginBottom: 10 }}
+                    fullWidth={true}
+                    label="Image Link"
+                    variant="outlined"
+                    onChange={(e) => {
+                        setImagelink(e.target.value)
+                    }}></TextField>
+                {/* <Typography variant="">Published</Typography>
+            <TextField onChange={(e) => {
+                setPublished(e.target.value)
+            }}></TextField> */}
+                <Button variant="contained" size="large" onClick={async() => {
+                    const newCourse = {
+                        title, description, price, imageLink,published:false
+                    }
+                    await axios.post(`${BASE_URL}/admin/addCourse`,newCourse,{
+                        headers:{
+                            'Authorization':'Bearer '+localStorage.getItem('token')
+                        }
+                    })
+                    alert("Successfully added course")
+                    navigate('/Admin/Courses')
+                }}>Add course</Button>
             </Card>
         </div>
-    )
-}
-function GrayTopper({ title }) {
-    return <div style={{ height: 250, background: "212121", top: 0, width: "100vw", zIndex: 0, marginBottom: -250 }}>
-        <div style={{ height: 250, display: "flex", justifyContent: "center", flexDirection: "column" }}>
-            <div>
-                <Typography style={{ color: "white", fontWeight: 600 }} variant="h3" textAlign={"center"}>
-                    {title}
-                </Typography>
-            </div>
-        </div>
-    </div>
-}
-function CourseCard({course}) {
-   
-    return <div style={{ display: "flex", marginTop: 50, justifyContent: "center", width: "100%" }}>
-        <Card style={{
-            margin: 10,
-            width: 350,
-            minHeight: 200,
-            borderRadius: 20,
-            marginRight: 50,
-            paddingBottom: 15,
-            zIndex: 2
-        }}>
-            <img src={course.imageLink} style={{ width: 350 }} ></img>
-            <div style={{ marginLeft: 10 }}>
-                <Typography variant="h5">{course.title}</Typography>
-                <Typography variant="subtitle2" style={{ color: "gray" }}>
-                    Price
-                </Typography>
-                <Typography variant="subtitle1">
-                    <b>Rs {course.price} </b>
-                </Typography>
-            </div>
-        </Card>
     </div>
 }
 
